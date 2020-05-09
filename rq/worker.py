4a135d891d7ee9cbb61358237518aaddaf7690c0
Sergio A. Mart <sergio@engineer.blue>
2020-05-09 15:57:43 +0100

    Log
    Signed-off-by: Sergio A. Mart <sergio@engineer.blue>

--- rq/worker.py
+++ rq/worker.py
@@ -55,6 +55,13 @@ blue = make_colorizer('darkblue')
 
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 class StopRequested(Exception):
